5557c3b13c86f7b77bcf902599531dab9293b092
Lleyton Fischer <lfischer11@hawkmail.hcc.edu>
2023-10-30 15:49:49 -0400

    First draft complete

--- exercise3_6/src/App.java
+++ exercise3_6/src/App.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter weight in pounds: ");
+        double lbs = scanner.nextDouble();
+
+        System.out.print("Enter feet for height: ");
+        int feet = scanner.nextInt();
+
+        System.out.print("Enter inches for height: ");
+        int inches = scanner.nextInt();
+
+        double bmi = lbs / Math.pow(feet*12 + inches, 2) * 703;
+
+        System.out.printf("BMI is %f\n", bmi);
+
+        if (bmi < 18.5) {
+            System.out.println("Underweight");
+        } else if (bmi < 24.9) {
+            System.out.println("Healthy Weight");
+        } else if (bmi < 29.9) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obese");
+        }
+        scanner.close();
     }
 }
