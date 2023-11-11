f382aae29f8a3ed49b232a69afafb8a5a46678a0
Lleyton Fischer <lfischer11@hawkmail.hcc.edu>
2023-11-11 15:55:06 -0500

    celcius -> celsius

--- exercise2_1/src/App.java
+++ exercise2_1/src/App.java
@@ -1,11 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         do {
-            System.out.print("Enter a degree in Celcius: ");
+            System.out.print("Enter a degree in Celsius: ");
             String in = System.console().readLine();
-            double celcius = Double.parseDouble(in);
-            double fahrenheit = (9.0 / 5.0) * celcius + 32;
-            System.out.printf("%.05g Celcius is %.05g Fahrenheit\n", celcius, fahrenheit);
+            double celsius = Double.parseDouble(in);
+            double fahrenheit = (9.0 / 5.0) * celsius + 32;
+            System.out.printf("%.05g Celsius is %.05g Fahrenheit\n", celsius, fahrenheit);
         } while (true);
     }
 }
