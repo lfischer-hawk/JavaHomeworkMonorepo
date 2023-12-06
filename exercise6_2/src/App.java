788a353a54ba25d1ecfe4a2b5d79ea1d2d42f7ec
Lleyton Fischer <lfischer11@hawkmail.hcc.edu>
2023-12-06 17:42:05 -0500

    Expand test calls

--- exercise6_2/src/App.java
+++ exercise6_2/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.printf("%d", sumDigits(234));
+        System.out.printf("sum of 234 is: %d\n", sumDigits(234));
+        System.out.printf("sum of 1390 is: %d\n", sumDigits(1390));
     }
 
     public static int sumDigits(long n) {
